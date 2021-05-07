0aae719cde17b8c851272a157688611c2ac4aad0
nareshpenchikala <70266217+nareshpenchikala@users.noreply.github.com>
2021-05-08 03:56:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+"""
+Routes and views for the flask application.
+"""
+"""
+The flask application package.
+"""
+
+from flask import Flask
+app = Flask(__name__)
+from flask import render_template,url_for
+import gunicorn
+
+@app.route('/')
+def home():
+    """Renders the home page."""
+    return render_template('s.html')
+
+
+@app.route("/Salesenquiry")
+def Salesenquiry():
+  return render_template("Salesenquiry.html")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ templates/feedback.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+Thank you <? php echo $_POST["Name"]; ?> for the feedback<br>
+<br>
+<? php echo $_POST["Message"]; ?>
+</body>
+</html>
