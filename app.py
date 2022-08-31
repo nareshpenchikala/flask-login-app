27063e404d7a82ab2468856c30657447727d3724
naresh P <penchikalanaresh62@gmail.com>
2022-08-31 13:59:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def login_page():
     error = None
     if request.method == 'POST':
         if request.form['username'] == 'admin' and request.form['password'] == 'admin':
-            return render_template('cafe-home.html')
+            return render_template('s.html')
         else:
             error = 'Invalid Credentials. Please try again.'
 
