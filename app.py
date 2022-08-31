bfcbc26fd38b0816d51b78eec4b22748cdf94887
naresh P <penchikalanaresh62@gmail.com>
2022-08-31 13:05:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 The flask application package.
 """
 
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import gunicorn
 
 app = Flask(__name__)
@@ -15,6 +15,13 @@ def home():
     """Renders the home page."""
     return render_template('s.html')
 
+@app.route('/home', methods=['GET', 'POST'])
+def form_example():
+    if 'admin' not in session:
+        return render_template('cafe-home.html')
+    else:
+        return redirect(url_for('login'))
+
 
 @app.route("/Salesenquiry", methods=['GET', 'POST'])
 def Salesenquiry():
@@ -23,11 +30,11 @@ def Salesenquiry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            session['admin'] = request.form['username']
+            return redirect(url_for('form_example'))
         else:
-            return redirect(url_for('home'))
+            error = 'Invalid Credentials. Please try again.'
     return render_template('login.html', error=error)
 
 
