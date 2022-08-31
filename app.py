823b4a057a9e9f08b56e37967ad66edfd855268c
naresh P <penchikalanaresh62@gmail.com>
2022-08-31 12:45:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ Routes and views for the flask application.
 The flask application package.
 """
 
-from flask import Flask
-app = Flask(__name__)
-from flask import render_template,url_for
+from flask import Flask, render_template, redirect, url_for, request
 import gunicorn
+
+app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -19,6 +19,16 @@ def home():
 @app.route("/Salesenquiry", methods=['GET', 'POST'])
 def Salesenquiry():
   return render_template("Salesenquiry.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 
 if __name__ == "__main__":
