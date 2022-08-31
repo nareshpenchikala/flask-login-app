f6a84dbc7cac8076fc3460d928d2debe8c046362
naresh P <penchikalanaresh62@gmail.com>
2022-08-31 13:37:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,31 +11,15 @@ import gunicorn
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    """Renders the home page."""
-    return render_template('s.html')
-
-@app.route('/home', methods=['GET', 'POST'])
-def form_example():
-    if 'admin' not in session:
-        return render_template('login.html')
-    else:
-        return render_template('cafe-home.html')
-
-
-@app.route("/Salesenquiry", methods=['GET', 'POST'])
-def Salesenquiry():
-  return render_template("Salesenquiry.html")
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+def login_page():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
             session['admin'] = request.form['username']
-            return redirect(url_for('form_example'))
+            return render_template('cafe-home.html')
+        else:
+            error = 'Invalid Credentials. Please try again.'
+
     return render_template('login.html', error=error)
 
 if __name__ == "__main__":
